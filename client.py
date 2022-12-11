73d91cf9da6d56897bbd5cc5f91efc37c800c4c2
0rang-3 <58794210+0rang-3@users.noreply.github.com>
2022-12-10 20:48:07 -0800

    Update client.py

--- client.py
+++ client.py
@@ -1,35 +1,17 @@
 import socket
-import random
 from threading import Thread
-from datetime import datetime
-from colorama import Fore, init, Back
 from time import sleep
-import csv
-from playsound import playsound
-
-init()
-status = ""
-online = False
-
-colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, 
-    Fore.LIGHTGREEN_EX, Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, Fore.LIGHTYELLOW_EX, 
-    Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
-]
-
-
-IP = "127.0.0.1"
+IP = "0.0.0.0"
 PORT = 5500
 separator_token = "<SEP>"
 
+client_sockets = set()
 swift = socket.socket()
+swift.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+swift.bind((IP, PORT))
+swift.listen(5)
 
-
-print(f"Connecting to {IP}:{PORT}...")
-swift.connect((IP, PORT))
-sleep(1)
-print("Connected!")
-
-print("Welcome to ")
+print("Welcome to")
 print("╭━━━╮╱╱╱╱╱╱╭━┳╮╱╭━━━┳╮╱╱╱╱╭╮")
 print("┃╭━╮┃╱╱╱╱╱╱┃╭╯╰╮┃╭━╮┃┃╱╱╱╭╯╰╮")
 print("┃╰━━┳╮╭╮╭┳┳╯╰╮╭╯┃┃╱╰┫╰━┳━┻╮╭╯")
@@ -37,141 +19,29 @@ print("╰━━╮┃╰╯╰╯┣╋╮╭┫┃╱┃┃╱╭┫╭╮┃�
 print("┃╰━╯┣╮╭╮╭┫┃┃┃┃╰╮┃╰━╯┃┃┃┃╭╮┃╰╮")
 print("╰━━━╯╰╯╰╯╰╯╰╯╰━╯╰━━━┻╯╰┻╯╰┻━╯")
 sleep(1)
-name = input("Enter your name to start: ")
+print(f"Watching {IP}:{PORT}")
 
-print("Choose color:")
-print("Options: RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA")
-color = input("> ").upper()
-
-if color == "RED":
-    client_color = Fore.RED
-elif color == "YELLOW":
-    client_color = Fore.YELLOW
-elif color == "GREEN":
-    client_color = Fore.GREEN
-elif color == "CYAN":
-    client_color = Fore.CYAN
-elif color == "BLUE":
-    client_color = Fore.BLUE
-elif color == "MAGENTA":
-    client_color = Fore.MAGENTA
-
-
-joined = name+" joined the chat"
-msg = f"{client_color}{joined}{Fore.RESET}"
-print("\n")
-swift.send(msg.encode())
-
-def listen_for_messages():
+def listen_for_client(lc):
     while True:
-        message = swift.recv(1024).decode()
-        print("\n" + message)
+        try:
+            message = lc.recv(1024).decode()
+        except Exception as i:
+            print(f"Error: {i}")
+            client_sockets.remove(lc)
+        else:
+            message = message.replace(separator_token, ": ")
+        for client_socket in client_sockets:
+            client_socket.send(message.encode())
 
-thread = Thread(target=listen_for_messages)
-thread.daemon = True
-thread.start()
 
 while True:
-    msg =  input()
-    if msg.lower() == "/commands":
-        print("Commands Menu\n+--------------+\n1. /partymode --> It's party time!\n2. /change name --> Changes your username on Swift Chat!\n3. /change color --> Change your text color on Swift Chat!\n4. /participants --> Shows the participants that are online\n5. /online --> Adds yourself to the participants list\n6. /offline --> Removes yourself from the participants list\n7. /set status --> Set a custom status that is visible in the participants list\n8. /remove status --> Removes your status from the participants list")
-        
-    elif msg.lower() == "/partymode":
-        partymode = "🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉"
-        msg = f"{client_color}{name} [{timestamp}] {separator_token} \n {partymode}{Fore.RESET}"
-        swift.send(msg.encode())
-    elif msg.lower() == "/change name":
-        newname = input("Enter your new name: ")
-        if(status == ""):
-            with open('participants.csv', 'r') as file:
-                data = file.read()
-                data = data.replace(name, newname)
-            with open('participants.csv', 'w') as file:
-                file.write(data)
-        else:
-            with open('participants.csv', 'r') as file:
-                data = file.read()
-                data = data.replace(name+" --> "+status, newname+" --> "+status)
-            with open('participants.csv', 'w') as file:
-                file.write(data)
-        name = newname
-    elif msg.lower() == "/change color":
-        print("Choose color:")
-        print("Options: RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA")
-        color = input("> ").upper()
-        if color == "RED":
-            client_color = Fore.RED
-        elif color == "YELLOW":
-            client_color = Fore.YELLOW
-        elif color == "GREEN":
-            client_color = Fore.GREEN
-        elif color == "CYAN":
-            client_color = Fore.CYAN
-        elif color == "BLUE":
-            client_color = Fore.BLUE
-        elif color == "MAGENTA":
-            client_color = Fore.MAGENTA
-    elif msg.lower() == "/participants":
-        with open('participants.csv', 'r') as file:
-            reader = csv.reader(file)
-            for x in reader:
-                print(x)
-    elif msg.lower() == "/online":
-        if(online == True):
-            print("You are already online.")
-        else:
-            if(status == ""):
-                with open('participants.csv', 'a') as file:
-                    file.write(name)
-            else:
-                with open('participants.csv', 'a')  as file:
-                    file.write(name+" --> "+status)
-            online = True
-            print("You are now online!")
-    elif msg.lower() == "/offline":
-        if(online == False):
-            print("You are already offline.")
-        else:
-            if(status == ""):
-                with open('participants.csv', 'r') as file:
-                    data = file.read()
-                    data = data.replace(name, "")
-                with open('participants.csv', 'w') as file:
-                    file.write(data)
-            else:
-                with open('participants.csv', 'r') as file:
-                    data = file.read()
-                    data = data.replace(name+" --> "+status, "")
-                with open('participants.csv', 'w') as file:
-                    file.write(data)
-            online = False
-        print('You are now offline!')
-    elif msg.lower() == "/set status":
-        print("What is your status?")
-        status = input("> ")
-        with open('participants.csv', 'r') as file:
-            data = file.read()
-            data = data.replace(name, name+" --> "+status)
-        with open('participants.csv', 'w') as file:
-            file.write(data)
-            print('Status is set!')
-    elif msg.lower() == "/remove status":
-        if(status == ""):
-            print('You do not have a current status set.')
-        else:
-            if(online == True):
-                with open('participants.csv', 'r') as file:
-                    data = file.read()
-                    data = data.replace(name+" --> "+status, name)
-                with open('participants.csv', 'w') as file:
-                    file.write(data)
-            status = ""
-            print("Your status has been removed.")
-        
-    else:
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M') 
-        msg = f"{client_color}{name} [{timestamp}] {separator_token} \n {msg}{Fore.RESET}"
-        swift.send(msg.encode())
-        playsound('/users/ruhanpandit/Downloads/Steel City Hackathon/swiftchatsound.mp3')
-    
+    client_socket, client_address = swift.accept()
+    print(f"[+] {client_address} connected.")
+    client_sockets.add(client_socket)
+    thread = Thread(target=listen_for_client, args=(client_socket,))
+    thread.daemon = True
+    thread.start()
+
+for lc in client_sockets:
+    lc.close()
 swift.close()
